3c2f75c07ecf54b273bfb47d5b99fde538ee7de5
Deepak Tiwari <deepak24294@gmail.com>
2020-05-02 03:53:26 +0000

    comments app

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,39 @@
 
 # A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="dheetiInterns",
+    password="Dsqlpass",
+    hostname="dheetiInterns.mysql.pythonanywhere-services.com",
+    databasename="dheetiInterns$comments",
+)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+
+
+@app.route("/", methods=["GET", "POST"])
+
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+
+    return redirect(url_for('index'))
