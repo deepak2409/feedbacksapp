d0e04372e3043646ad8348b50de8d327aa2b9e50
Deepak Tiwari <deepak24294@gmail.com>
2020-05-02 05:59:16 +0000

    Working app with storing in db done

--- flask_app.py
+++ flask_app.py
@@ -24,6 +24,19 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
+class Feed(db.Model):
+
+    __tablename__ = "feed"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    idno = db.Column(db.String(30))
+    email = db.Column(db.String(30))
+    phno = db.Column(db.String(30))
+    feed = db.Column(db.String(4096))
+
+
+
 
 
 
@@ -31,9 +44,20 @@ class Comment(db.Model):
 
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
-    comment = Comment(content=request.form["contents"])
-    db.session.add(comment)
-    db.session.commit()
+        return render_template("main_page.html")
+    if request.method == "POST":
+        name = request.form['name']
+        idno = request.form['idno']
+        email = request.form['email']
+        phno = request.form['phone']
+        feeds = request.form['feed']
+        feed = Feed(name= name, idno = idno ,email = email,phno = phno,feed = feeds)
+        db.session.add(feed)
+        db.session.commit()
+        return "Success"
 
-    return redirect(url_for('index'))
+@app.route('/allfeedback', methods = ['GET'])
+def feed():
+
+        return render_template('feedbacks.html', feeds =Feed.query.all())
+
