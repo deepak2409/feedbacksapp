190b71f9b094bdeb1d29b59878accfba8b371818
Deepak Tiwari <deepak24294@gmail.com>
2020-05-07 17:08:05 +0000

    Users table working from database

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, LoginManager, UserMixin,logout_user,login_required,current_user
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_migrate import Migrate
 app = Flask(__name__)
 
 app.config["DEBUG"] = True
@@ -18,34 +19,33 @@ app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.secret_key = "something only you know"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-class User(UserMixin):
+class User(UserMixin, db.Model):
 
-    def __init__(self, username, password_hash):
-        self.username = username
-        self.password_hash = password_hash
+    __tablename__ = "users"
 
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-
     def get_id(self):
         return self.username
 
-all_users = {
-    "admin": User("admin", generate_password_hash("secret")),
-    "bob": User("bob", generate_password_hash("less-secret")),
-    "caroline": User("caroline", generate_password_hash("completely-secret")),
-}
+# all_users = {
+#     "admin": User("admin", generate_password_hash("secret")),
+#     "bob": User("bob", generate_password_hash("less-secret")),
+#     "caroline": User("caroline", generate_password_hash("completely-secret")),
+# }
+
 @login_manager.user_loader
 def load_user(user_id):
-    return all_users.get(user_id)
-
+    return User.query.filter_by(username=user_id).first()
 class Comment(db.Model):
 
     __tablename__ = "comments"
@@ -64,6 +64,7 @@ class Feed(db.Model):
     phno = db.Column(db.String(30))
     grade = db.Column(db.String(30))
     feed = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default="5/7/2020")
 
 
 
@@ -99,11 +100,11 @@ def feeds():
 def login():
     if request.method == "GET":
         return render_template("login_page.html", error=False)
-
-    username = request.form["username"]
-    if username not in all_users:
+    user = load_user(request.form["username"])
+    if user is None:
         return render_template("login_page.html", error=True)
-    user = all_users[username]
+    # Check this portion of code
+    # user = all_users[username]
 
     if not user.check_password(request.form["password"]):
         return render_template("login_page.html", error=True)
